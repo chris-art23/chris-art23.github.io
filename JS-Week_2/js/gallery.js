8bfe4f3e49e887dc85105ffc5f8788327fcb238e
Christopher Arteaga <131785381+chris-art23@users.noreply.github.com>
2025-04-08 00:23:37 -0400

    Add files via upload

---
+++ JS-Week_2/js/gallery.js
@@ -0,0 +1,13 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    document.getElementById("image").style.backgroundImage = "url(" + previewPic.src + ")";
+    document.getElementById("image").style.backgroundColor = "#000000";
+    document.getElementById("image").innerHTML = previewPic.alt;
+}
+   
+function unDo(){
+    document.getElementById("image").style.backgroundImage = "url('')";
+    document.getElementById("image").style.backgroundColor = "#8e68ff";
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
